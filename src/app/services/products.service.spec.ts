4e94ed954f9a6620a4c8d52c98f3d447c87c1542
C1329250 Rafael Ferreira De Araujo <c1329250@interno.bb.com.br>
2024-08-16 16:34:45 -0300

    test(products.service): implementa testes unitarios dos metodos do service de produtos.

--- src/app/services/products.service.spec.ts
+++ src/app/services/products.service.spec.ts
@@ -5,28 +5,86 @@ import {
   HttpClientTestingModule,
   HttpTestingController,
 } from '@angular/common/http/testing';
+import { Product } from '../models/product.model';
+import { environment } from 'src/environments/environment';
 
 describe('ProductsService', () => {
   let service: ProductsService;
   let httpController: HttpTestingController;
+  let baseAPI: string;
+  let product: Product;
 
   beforeEach(() => {
+    baseAPI = environment.baseAPI;
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(ProductsService);
     httpController = TestBed.inject(HttpTestingController);
+    product = {
+      id: '1',
+      title: 'Product Title',
+      price: '20',
+      description: 'Product Description',
+      category: 'Smartwatches',
+    };
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should test getProducts', () => {});
+  it('should test getProducts', () => {
+    const products: Product[] = [{ ...product }];
 
-  it('should test saveProducts', () => {});
+    service.getProducts().subscribe((response) => {
+      expect(response).toEqual(products);
+    });
 
-  it('should test updateProduct', () => {});
+    const req = httpController.expectOne({
+      method: 'GET',
+      url: `${baseAPI}products`,
+    });
 
-  it('should test deleteProduct', () => {});
+    req.flush(products);
+  });
+
+  it('should test saveProducts', () => {
+    service.saveProduct(product).subscribe((response) => {
+      expect(response).toEqual(product);
+    });
+
+    const req = httpController.expectOne({
+      method: 'POST',
+      url: `${baseAPI}products`,
+    });
+
+    req.flush(product);
+  });
+
+  it('should test updateProduct', () => {
+    service.updateProduct(product).subscribe((response) => {
+      expect(response).toEqual(product);
+    });
+
+    const req = httpController.expectOne({
+      method: 'PUT',
+      url: `${baseAPI}products/${product.id}`,
+    });
+
+    req.flush(product);
+  });
+
+  it('should test deleteProduct', () => {
+    service.deleteProduct(Number(product.id)).subscribe((response) => {
+      expect(response).toEqual(product);
+    });
+
+    const req = httpController.expectOne({
+      method: 'DELETE',
+      url: `${baseAPI}products/${product.id}`,
+    });
+
+    req.flush(product);
+  });
 });
