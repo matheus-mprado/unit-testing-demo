ea9db1ff077a7bf621a88ea4e403b554ebb20ed5
PauloRoque19 <55522424+PauloRoque19@users.noreply.github.com>
2024-08-21 16:27:59 -0300

    Update products.service.spec.ts

--- src/app/services/products.service.spec.ts
+++ src/app/services/products.service.spec.ts
@@ -5,6 +5,8 @@ import {
   HttpClientTestingModule,
   HttpTestingController,
 } from '@angular/common/http/testing';
+import { Product } from '../models/product.model';
+import { environment } from 'src/environments/environment';
 
 describe('ProductsService', () => {
   let service: ProductsService;
@@ -22,11 +24,81 @@ describe('ProductsService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should test getProducts', () => {});
+  it('should test getProducts', () => {
+    const mockProducts: Product[] = [
+        {
+        id: '1',
+        title: 'Test Product',
+        description: 'Test description',
+        price: '19.99',
+        category: 'Test category'
+      },
+      {
+        id: '2',
+        title: 'Test Product',
+        description: 'Test description',
+        price: '20.99',
+        category: 'Test category'
+      }
+    ];
 
-  it('should test saveProducts', () => {});
+    service.getProducts().subscribe((products) => {
+      expect(products).toEqual(mockProducts);
+    });
 
-  it('should test updateProduct', () => {});
+    const req = httpController.expectOne(`${environment.baseAPI}products`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockProducts);
+  });
 
-  it('should test deleteProduct', () => {});
+  it('should test saveProduct', () => {
+    const mockProduct: Product = {
+      id: '1',
+      title: 'Test Product',
+      description: 'Test description',
+      price: '19.99',
+      category: 'Test category'
+    };
+
+    service.saveProduct(mockProduct).subscribe((savedProduct) => {
+      expect(savedProduct).toEqual(mockProduct);
+    });
+
+    const req = httpController.expectOne(`${environment.baseAPI}products`);
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(mockProduct);
+    req.flush(mockProduct);
+  });
+
+  it('should test updateProduct', () => {
+   const mockProduct: Product = {
+      id: '1',
+      title: 'Test Product',
+      description: 'Test description',
+      price: '19.99',
+      category: 'Test category2'
+    };
+
+    service.updateProduct(mockProduct).subscribe((updatedProduct) => {
+      expect(updatedProduct).toEqual(mockProduct);
+    });
+
+    const req = httpController.expectOne(`${environment.baseAPI}products/${mockProduct.id}`);
+    expect(req.request.method).toBe('PUT');
+    expect(req.request.body).toEqual(mockProduct);
+    req.flush(mockProduct);
+  });
+
+  it('should test deleteProduct', () => {
+    const productId = 1;
+
+    service.deleteProduct(productId).subscribe((deletedProduct) => {
+      expect(Number(deletedProduct.id)).toBe(productId);
+    });
+
+    const req = httpController.expectOne(`${environment.baseAPI}products/${productId}`);
+    expect(req.request.method).toBe('DELETE');
+    req.flush({ id: productId });
+  });
+
 });
