355f5d414d36baac8392a2ed4ec6445f77ec8723
PauloRoque19 <55522424+PauloRoque19@users.noreply.github.com>
2024-08-21 16:31:06 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,12 @@ export class AppComponent {
 
   constructor() {}
 
+  /**
+   * Adds two numbers.
+   * @param a The first number.
+   * @param b The second number.
+   * @returns The sum of the two numbers.
+   */
   add(a: number, b: number) {
     return a + b;
   }
