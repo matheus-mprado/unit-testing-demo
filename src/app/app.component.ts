0a6edf1c87b15ed5e419a32947a326aea661b4ee
Luiz Henrique Cavalcanti <luizhsc.araujo@outlook.com>
2024-08-20 23:25:56 -0300

    adicionando comentário e testes para o component AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
   constructor() {}
-
+  /**
+   * add - Soma dois valores numéricos e retorna o resultado no tipo number
+   * @param a primeiro valor no tipo numérico
+   * @param b segundo valor no tipo numérico
+   * @returns resultado da soma dos dois valores
+   */
   add(a: number, b: number) {
     return a + b;
   }
